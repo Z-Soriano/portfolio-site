8ae5879122170f19625dc1454294de4919015fc0
Zachary <z.dsd2023@gmail.com>
2024-10-29 16:56:34 -0700

    fix image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,10 @@ export default function Home() {
         <div>
             <main>
                 <h3>Purple</h3>
-                <h1>Hi</h1>
             </main>
 
             <a href="https://github.com/Z-Soriano">
-                <Image src="/purple.png" alt="GitHub profile for Z-Soriano" width={60} height={60} className="image" />
+                <Image src="/purple.png" alt="x" width={60} height={60} className="image" />
             </a>
         </div>
 
