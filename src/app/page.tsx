001190378e74fa21c29986419a372b6a500fb9fa
Zachary <z.dsd2023@gmail.com>
2025-05-26 09:43:07 -0700

    updated style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,57 @@ export default function Home() {
     return (
         <div>
             <main>
-                <h3>Purple</h3>
+                <h3>Free Fabian</h3>
+
             </main>
 
-            <a href="https://github.com/Z-Soriano">
-                <Image src="/purple.png" alt="x" width={60} height={60} className="image" />
-            </a>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <a href="https://github.com/Z-Soriano" target="_blank" className="image-link">
+                    <Image src="/Pfp.png" alt="x" width={200} height={200} className="image"/>
+                </a>
+            </div>
+
+            <div className="row">
+                <div className="column">
+                    <div className="image-wrapper">
+                        <img src="/gametest1.png" className="projImage"/>
+                        <div className="overlay-group">
+                            <h1 className="overlay-title">Project #1</h1>
+                            <p className="overlay-subtitle">2D Platformer</p>
+                            <button className="overlay-button">View Project</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="column">
+                    <div className="image-wrapper">
+                        <img src="/purple.png" className="projImage"/>
+                        <div className="overlay-group">
+                            <h1 className="overlay-title">About Me</h1>
+                            <p className="overlay-subtitle">Second Year CS Major Stat Minor @SDSU</p>
+                            <p className="overlay-subtitle">GPA: 3.97</p>
+                            <p className="overlay-subtitle">Notable Courses:</p>
+                            <p className="overlay-subtitle">Calculus II, Linear Algebra, Discrete Math, Data
+                                Structures</p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="column">
+                    <div className="image-wrapper">
+                        <img src="/purple.png" className="projImage"/>
+                        <div className="overlay-group">
+                            <h1 className="overlay-title">Skills</h1>
+                            <p className="overlay-subtitle">- I'm proficient in Java and Python</p>
+                            <p className="overlay-subtitle">- I've been learning JS CSS HTML and Godot</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
 
     );
 }
